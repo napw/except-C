7d5bb57bd13f97855c7c2fa9a62386807c9b1361
napw <wpans@live.com>
2018-09-30 14:31:28 +0800

    TRY/THROW/CATCH done

---
+++ excepC.h
@@ -0,0 +1,57 @@
+//
+// Created by wpans on 2018/9/29.
+//
+
+#ifndef SETJMP_EXCEPC_H
+#define SETJMP_EXCEPC_H
+
+#include <setjmp.h>
+
+typedef struct ExceptMessage {
+    char *message;
+} ExceptMessage;
+typedef struct Except {
+    struct Except *Prev;
+    jmp_buf env;
+    const char *FileName;
+    int Line;
+    const ExceptMessage *What;
+
+} Except;
+
+enum {
+    ExceptEntered = 0, ExceptRaised, ExceptHandled, ExceptFinalized
+};
+
+extern Except *ExceptStack;
+extern const ExceptMessage AssertFailed;
+
+void RaiseExcept(const ExceptMessage *m, const char *filename, int line);
+
+#define THROW(m) RaiseExcept(&(m),__FILE__,__LINE__)
+#define RERAISE RaiseExcept(except.What,except.FileName,except.Line)
+#define RETURN switch(ExceptStack=ExceptStack->Prev,0){default:return}
+#define CANCELUNRAISEDEXCEPTION if(ExceptFlag==ExceptEntered){ ExceptStack=ExceptStack->Prev;}
+
+#define TRY do{\
+    Except except;\
+    except.Prev=ExceptStack;\
+    ExceptStack=&except;\
+    volatile int ExceptFlag=setjmp(except.env);\
+    if (ExceptFlag==ExceptEntered){\
+
+#define CATCH(e)\
+        CANCELUNRAISEDEXCEPTION\
+    }else if(except.What==&(e)){\
+        ExceptFlag=ExceptHandled;\
+
+#define ENDTRY\
+        CANCELUNRAISEDEXCEPTION\
+    }\
+    if(ExceptFlag==ExceptRaised){\
+        RERAISE;\
+    }\
+}while(0);
+
+
+#endif //SETJMP_EXCEPC_H
---
+++ exceptC.c
@@ -0,0 +1,33 @@
+//
+// Created by wpans on 2018/9/29.
+//
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "excepC.h"
+
+Except *ExceptStack = NULL;
+
+void RaiseExcept(const ExceptMessage *m, const char *filename, int line) {
+    Except *latest = ExceptStack;
+    assert(m);
+    if (latest == NULL) {
+        fprintf(stderr, "Uncaught exception");
+        if (m->message != NULL) {
+            fprintf(stderr, " %s", m->message);
+        } else {
+            fprintf(stderr, " at 0x%p", m);
+        }
+        if (filename != NULL && line > 0) {
+            fprintf(stderr, " raised at %s:%d\n", filename, line);
+        }
+        fprintf(stderr, "program aborting\n");
+        fflush(stderr);
+        abort();
+    }
+    latest->What = m;
+    latest->FileName = filename;
+    latest->Line = line;
+    ExceptStack = ExceptStack->Prev;
+    longjmp(latest->env, ExceptRaised);
+}
